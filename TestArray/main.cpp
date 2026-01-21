b5d8591baa54c399b29477380aaf4be1c9a92bd1
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2026-01-21 08:51:21 +0000

    trovaMax

--- TestArray/main.cpp
+++ TestArray/main.cpp
@@ -9,8 +9,14 @@ using namespace std;
 #define DIM 10
 
 
-void trovaMax( int m[DIM][DIM] ) {
-    // .............
+int trovaMax( int m[DIM][DIM] ) {
+    int max = m[0][0];
+    for (int i=0; i<DIM; i++) {
+        for (int j=0; j<DIM; j++) {
+            if (m[i][j] > max) max = m[i][j];
+        }
+    }
+    return max;
 }
 
 void stampaMatrice( int m[DIM][DIM] ) {
@@ -34,6 +40,6 @@ int main()
     int matrice[DIM][DIM];
     riempiMatrice(matrice);
     stampaMatrice(matrice);
-
+    cout << "Il massimo e': " << trovaMax(matrice) << endl;
     return 0;
 }
