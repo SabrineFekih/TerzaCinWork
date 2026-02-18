e8ce43ef08e3328c14a5b0e678895ebc4f9ed22f
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2026-02-18 08:28:53 +0000

    insertionsort

--- SortArray/main.cpp
+++ SortArray/main.cpp
@@ -29,7 +29,7 @@ void bubblesort( int vett[], int len, int& confronti, int& scambi ) {
     confronti = scambi = 0;
     for (int i=0; i<len; i++)
         for (int j=0; j<len-1; j++, confronti++)
-            if (vett[j]>vett[j+1]) {
+            if ( vett[j] > vett[j+1] ) {
                 scambi++;
                 scambia(vett[j], vett[j+1]);
             }
@@ -49,21 +49,42 @@ void bubblesort_con_guardia( int vett[], int len, int& confronti, int& scambi ) 
     }
 }
 
+void insertionsort( int vett[], int len, int& confronti, int& scambi ) {
+    confronti = scambi = 0;
+    for (int i=1; i<len; i++) {
+        int value = vett[i];
+        int j = i-1;
+        while (j>=0 && vett[j]>value) {
+            confronti++;
+            scambi++;
+            vett[j+1] = vett[j];
+            j--;
+        }
+        vett[j+1] = value;
+    }
+}
+
 int main()
 {
+
     const int N = 10;
-    int v[N], w[N], c, s;
+    int v[N], w[N], x[N], c, s;
     srand(time(NULL));
 
     randomFill(v, N);
-    stampa(v, N);
     clona(v, w, N);
+    clona(v, x, N);
+    cout << "Vettore originale: ";
+    stampa(v, N);
     bubblesort(v, N, c, s);
     cout << "Bubblesort: " << c << " confronti e " << s << " scambi:" << endl;
     stampa(v, N);
     bubblesort_con_guardia(w, N, c, s);
     cout << "Bubblesort con guardia: " << c << " confronti e " << s << " scambi:" << endl;
     stampa(w, N);
+    insertionsort(x, N, c, s);
+    cout << "Insertionsort: " << c << " confronti e " << s << " scambi:" << endl;
+    stampa(x, N);
     cout << endl;
 
     return 0;
